31b9943b1568c54283b23b18756d3c42b5d926b3
Hans Bentlöv <hanserikb@gmail.com>
2012-11-28 14:17:59 +0100

    Lägga till tomt rails projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wp102dv401::Application.config.secret_token = '18b7ccca44e04bea102732a5ff1920690e64025d4318f940d98a5acd6ab4b30d279e60255cdab93e32cb8100311f0fda4e2ce69edc8a3ab29871bb8b5a996d10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wp102dv401::Application.config.session_store :cookie_store, key: '_wp102dv401_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wp102dv401::Application.config.session_store :active_record_store
