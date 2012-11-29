5957c8cc813ef30bb9543673281448da43621235
Claes Tillborg <claes_tillborg@live.se>
2012-11-29 14:53:13 +0100

    Omdirigera index för in- och ut loggad

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Wp102dv401::Application.routes.draw do
   get "home/index"
 
   devise_for :users
+  resources :dashboard
+  root to: "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
