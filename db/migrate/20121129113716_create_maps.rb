22763730bbbfd6cbfa6801c2189b25dae83bcd1b
Linus Persson <linus@870621.se>
2012-11-29 12:43:39 +0100

    Lägga till Map-modellen

---
+++ db/migrate/20121129113716_create_maps.rb
@@ -0,0 +1,11 @@
+class CreateMaps < ActiveRecord::Migration
+  def change
+    create_table :maps do |t|
+      t.string :name
+      t.text :description
+      t.string :creator
+
+      t.timestamps
+    end
+  end
+end
