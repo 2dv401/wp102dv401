db7af881d7e316cd8b8038735397eb21cc12cc29
Linus Persson <linus@870621.se>
2012-12-02 14:30:43 +0100

    Uppdatera .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -31,9 +31,6 @@ ActiveRecord::Schema.define(:version => 20121129121350) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
-end
-
-ActiveRecord::Schema.define(:version => 20121129110024) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -52,4 +49,5 @@ ActiveRecord::Schema.define(:version => 20121129110024) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
 end
