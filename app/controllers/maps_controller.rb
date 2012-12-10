c947f7223e334d615e566aac31ff6c604a1a7bb7
Martin Svensson <ms87pn@gmail.com>
2012-12-10 13:08:13 +0100

    Modell/Controller för map
    Skapat modell för map och börjat på controller för map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,6 +3,18 @@ class MapsController < ApplicationController
   end
 
   def new
+		#todo: kontrollera ifall användaren är inloggad
+		
+		map = Map.new
+		map.name = params[:map][:name]
+		map.description = params[:map][:description]
+		map.private = params[:map][:private]
+		#todo:userid?
+		
+		#todo:validera?
+		
+		map.save
+		redirect_to :controller => "maps" ,:action => "show", :id => map.id
   end
 
   def edit
@@ -12,5 +24,7 @@ class MapsController < ApplicationController
   end
 
   def delete
+	#todo:kontrollera ifall användaren äger kartan
+	Map.find_by_id(params[:id]).destroy
   end
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,14 @@
 class Map < ActiveRecord::Base
-  attr_accessible :creator, :description, :name
+	belongs_to :user
+	has_many :map_places
+	
+	validates_presence_of :map_id, :message => "Map could not be found!"
+	
+	validates_presence_of :user_id, :message => "User could not be found!"
+	
+	validates_presence_of :description, :message => "Last name can't be empty!"
+	validates_length_of :description, :maximum=> 45 , :message => "Description can't be longer than 30 characters!"
+	
+	validates_presence_of :private, :message => ""
+	
 end
