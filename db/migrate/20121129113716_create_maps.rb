c94ecc1c94055cfb34040235e25cb74a78bca4ba
Hans Bentlöv <hanserikb@gmail.com>
2012-12-11 16:30:57 +0100

    Update db/migrate/20121129113716_create_maps.rb
    Ändra datatyp typo

--- db/migrate/20121129113716_create_maps.rb
+++ db/migrate/20121129113716_create_maps.rb
@@ -3,8 +3,8 @@ class CreateMaps < ActiveRecord::Migration
     create_table :maps do |t|
       t.string :name, :null => false, :limit => 50
       t.text :description, :null => false, :limit => 256
-      t.private :private, :null => false
-      t.int :user_id, :null => false
+      t.boolean :private, :null => false
+      t.integer :user_id, :null => false
       t.timestamps
     end
   end
