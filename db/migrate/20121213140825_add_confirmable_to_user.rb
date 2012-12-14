69f8a316a01e73cb7a82752c3c5f4e708d3ea438
Claes Tillborg <claes_tillborg@live.se>
2012-12-14 18:56:00 +0100

    lägg till connfirmed_at på alla existerande användare

--- db/migrate/20121213140825_add_confirmable_to_user.rb
+++ db/migrate/20121213140825_add_confirmable_to_user.rb
@@ -5,5 +5,6 @@ class AddConfirmableToUser < ActiveRecord::Migration
     add_column :users, :confirmation_sent_at, :datetime
 
     add_index :users, :confirmation_token,   :unique => true
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121213140825) do
+ActiveRecord::Schema.define(:version => 20121214174515) do
 
   create_table "location_images", :force => true do |t|
     t.string   "file_name",   :limit => 32,  :null => false
