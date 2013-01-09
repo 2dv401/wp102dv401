e28022686bc907d7166a3e298053e575b562de7a
Claes Tillborg <claes_tillborg@live.se>
2013-01-09 14:54:17 +0100

    ändra tillbaka status begränsningen till 5120

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -6,7 +6,7 @@ class StatusUpdate < ActiveRecord::Base
 
 	attr_accessible :content, :map_id, :user_id
 
-  validates :content, presence: true, length: { :maximum => 2 }
+  validates :content, presence: true, length: { :maximum => 5120 }
   validates :map_id, presence: true
   validates :user_id, presence: true
 end
