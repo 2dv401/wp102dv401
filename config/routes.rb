6e00c6d88d8d23af49146ac38a9c6fa40d3ce543
Claes Tillborg <claes_tillborg@live.se>
2013-01-11 14:58:14 +0100

    skapa tillfällig användarvillkor-sida

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Wp102dv401::Application.routes.draw do
 
   get "/om-kartr" => "pages#about", :as => :pages_about
-  get "/anvandarvillkar" => "pages#terms", :as => :pages_terms
+  get "/anvandarvillkor" => "pages#terms", :as => :pages_terms
   get "/sekretess" => "pages#privacy", :as => :pages_privacy
   get "/hjalp" => "pages#help", :as => :pages_help
 
