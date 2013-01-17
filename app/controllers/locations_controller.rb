00af0a87fa0fe36881850cc83122d500a38d8dae
Claes Tillborg <claes_tillborg@live.se>
2013-01-17 17:07:15 +0100

    lägg till så att location återanvänds

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,12 @@
 class LocationsController < ApplicationController
   def create
+    @location = Location.find_by_latitude_and_longitude(@map.latitude,@map.longitude)
+    # Om inte positionen finns skapas den
+    unless @location.any?
+      @location = Location.new do |l|
+        l.longitude = @map.longitude
+        l.latitude = @map.latitude
+      end
   end
 
   def destroy
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,59 +2,43 @@ class MapsController < ApplicationController
   before_filter :authenticate_user!
   def index
 
-   @maps = Map.find(:all, :conditions => [ "user_id = ?", current_user.id])
+    @maps = Map.find(:all, :conditions => [ "user_id = ?", current_user.id])
 
- end
+  end
 
- def follow
+  def follow
     @map = Map.find(params[:map_id])
     current_user.follow!(@map)
     render :template => 'maps/follow/toggle'
- end
+  end
 
- def unfollow
+  def unfollow
     @map = Map.find(params[:map_id])
     current_user.unfollow!(@map)
     render :template => 'maps/follow/toggle'
- end
- def show
-  #Nya objekt som kommer finnas på maps-sidan
-  @status_update = StatusUpdate.new
-  @status_comment = StatusComment.new
-
-  @map_comment = MapComment.new
-
-    #todo: kontrollera ifall anv�ndaren ska f� se kartan
-    #todo: kontrollera att kartan finns
-    #@map =  Map.find(:all, :conditions => [ "id = ?",  params[:id]])
-
-  # Referens till ett Map-objekt
-  @map = Map.find(params[:id])
-
-  if request.path != map_path(@map)
-    redirect_to @map, status: :moved_permanently
   end
+  def show
+    #Nya objekt som kommer finnas på maps-sidan
+    @status_update = StatusUpdate.new
+    @status_comment = StatusComment.new
+    @map_comment = MapComment.new
 
-  @locations = @map.locations
+    # Referens till ett Map-objekt
+    @map = Map.find(params[:id])
+    puts @map
+    if request.path != map_path(@map)
+      redirect_to @map, status: :moved_permanently
+    end
 
-  # Referens till ett gmaps-objekt
-  if @locations.any?
+    @marks = @map.marks
+    # Referens till ett gmaps-objekt
+    if @marks.any?
+     # @display_map = @marks.to_gmaps4rails
+    else
+      @display_map = @map.to_gmaps4rails
+    end
 
-  	#G�r om startpunkten till en location
-    @location = Location.new
-  	@location.name = @map.name
-  	@location.description = @map.description
-  	@location.latitude = @map.latitude
-  	@location.longitude = @map.longitude
-  	@location.location_type = LocationType.new(:name => "Startpunkt")
-  	@location.save
-  	@locations << @location
-    @display_map = @locations.to_gmaps4rails
-  else
-    @display_map = @map.to_gmaps4rails
   end
-  
-end
 
   def new
     @map = Map.new
@@ -67,84 +51,100 @@ end
   end
 
   def create
-      
-      @map = Map.new(params[:map])
+
+    @map = Map.new(params[:map])
+    @map_options = get_map_options
+    @map.gmaps = true
+    @map.user = current_user
+    puts @map
+
+    # Om kartan sparas
+    if @map.save
+      # Skapa ett location-objekt på kartans position om det inte redan finns
+      @location = Location.find_or_create_by_latitude_and_longitude(@map.latitude, @map.longitude)
+
+      @mark = Mark.new do |m|
+        m.map = Map.find(@map)
+        m.location = @location
+        m.name = @map.name
+        m.description = @map.description
+
+        # @location.location_type = LocationType.new(:name => "Startpunkt")
+      end
+
+      if @mark.save
+        flash[:notice] = "Kartan och positionen sparades!"
+      else
+        flash[:notice] = "Kartan sparades! men inte positionen"
+      end
+      redirect_to map_path(@map)
+    else
+      flash[:error] = "Fel intraffade nar kartan skulle sparas."
+      render :action => "new"
+    end
+
+  end
+
+  # GET /maps/:slug/edit
+  def edit
+    @map = Map.find(params[:id])
+    if current_user == @map.user
+
       @map_options = get_map_options
-      @map.gmaps = true
-      @map.user_id = current_user.id
-      
-      puts @map
 
-        if @map.save
-          flash[:notice] = "Kartan sparades!"
-          redirect_to map_path(@map)
-        else
-          flash[:error] = "Fel intraffade nar kartan skulle sparas."
-          render :action => "new"
-        end
+    else
+      flash[:notice] = "Fel, bara agaren till kartan kan andra den."
+      redirect_to map_path(@map)
+    end
+  end
 
+  # PUT /maps/:slug/edit
+  def update
+    @map = Map.find(params[:id])
+    if current_user == @map.user
+
+      @map_options = get_map_options
+
+      if @map.update_attributes(params[:map])
+        flash[:notice] = "Kartan sparades!"
+        redirect_to map_path(@map)
+      else
+        flash[:error] = "Fel intraffade nar kartan skulle sparas."
+        render :action => "edit"
       end
+    else
+      flash[:notice] = "Fel, bara agaren till kartan kan uppdatera den."
+    end
+  end
 
-      # GET /maps/:slug/edit
-      def edit
-        @map = Map.find(params[:id])
-        if current_user == @map.user
-
-          @map_options = get_map_options
-
-        else
-          flash[:notice] = "Fel, bara agaren till kartan kan andra den."
-          redirect_to map_path(@map)
-        end
+  def destroy
+    @map = Map.find(params[:id])
+    if current_user == @map.user
+      if @map.destroy
+        flash[:notice] = "Kartan borttagen"
+      else
+        flash[:notice] = "Fel nar kartan skulle tagas bort"
       end
+    else
+      flash[:notice] = "Fel, bara agaren till kartan kan ta bort den."
+    end
+    redirect_to root_path
+  end
 
-      # PUT /maps/:slug/edit
-      def update
-        @map = Map.find(params[:id])
-        if current_user == @map.user
+  # Sets options for map
+  def get_map_options
 
-          @map_options = get_map_options
-
-          if @map.update_attributes(params[:map])
-            flash[:notice] = "Kartan sparades!"
-            redirect_to map_path(@map)
-          else
-            flash[:error] = "Fel intraffade nar kartan skulle sparas."
-            render :action => "edit"
-          end
-        else
-          flash[:notice] = "Fel, bara agaren till kartan kan uppdatera den."
-        end
-      end
-
-      def destroy
-       @map = Map.find(params[:id])
-       if current_user == @map.user
-         if @map.destroy
-           flash[:notice] = "Kartan borttagen"
-         else
-           flash[:notice] = "Fel nar kartan skulle tagas bort"
-         end
-       else
-         flash[:notice] = "Fel, bara agaren till kartan kan ta bort den."
-       end
-       redirect_to root_path
-     end
-     
-     # Sets options for map
-     def get_map_options
-       
-        return  {
-          "map_options" => {
+    return  {
+        "map_options" => {
             "auto_zoom" => false,
-            "zoom" => 8,
+            "zoom" => 6,
             "center_latitude" => @map.latitude,
             "center_longitude" => @map.longitude
-            },
-            "markers" => {
-              "data" => @map.to_gmaps4rails
-            }
+        },
+        "markers" => {
+            "data" => @map.to_gmaps4rails
         }
-        	
-     end
+    }
+
   end
+end
