0e2c6853f1ea5fd804baf9543c1527fcfa2c4b05
Claes Tillborg <claes_tillborg@live.se>
2013-01-24 17:55:23 +0100

    ajaxifiera likeknappen på statuskommentarerna

--- app/controllers/status_comments_controller.rb
+++ app/controllers/status_comments_controller.rb
@@ -2,6 +2,8 @@ class StatusCommentsController < ApplicationController
   before_filter :authenticate_user!
 
   def toggle_like
+    @map = Map.find(params[:map_id])
+    @update = StatusUpdate.find(params[:status_update_id])
     @status_comment = StatusComment.find(params[:status_comment_id])
 
     if current_user.likes?(@status_comment)
@@ -10,16 +12,16 @@ class StatusCommentsController < ApplicationController
       current_user.like!(@status_comment)
     end
 
-    redirect_to map_path(params[:map_id])
+    render :template => 'status_comments/like/toggle'
   end
 
   def create
     #Skapar ny statusuppdatering från post-parametrarna samt lägger till aktuella användaren
-    @comment = StatusComment.new(params[:status_comment])
-    @comment.user = current_user
-    @comment.status_update = StatusUpdate.find(params[:status_update_id])
+    @status_comment = StatusComment.new(params[:status_comment])
+    @status_comment.user = current_user
+    @status_comment.status_update = StatusUpdate.find(params[:status_update_id])
 
-    if @comment.save
+    if @status_comment.save
       flash[:notice] = "Kommentaren sparad"
     else
       flash[:notice] = "Fel nar kommentaren skulle sparas"
@@ -28,9 +30,9 @@ class StatusCommentsController < ApplicationController
   end
 
   def destroy
-    @comment = StatusComment.find(params[:id])
-    if current_user == @comment.user || current_user == @comment.status_update.map.user
-      if @comment.destroy
+    @status_comment = StatusComment.find(params[:id])
+    if current_user == @status_comment.user || current_user == @status_comment.status_update.map.user
+      if @status_comment.destroy
         flash[:notice] = "Kommentaren borttagen"
       else
         flash[:notice] = "Fel nar kommentaren skulle tagas bort"
