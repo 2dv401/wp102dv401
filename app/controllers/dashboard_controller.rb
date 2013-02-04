2ed2a86e486a92b37fdf10d5d87edffc02a49264
Hans Bentlöv <hanserikb@gmail.com>
2013-02-04 16:45:56 +0100

    korta ned sökfrågan

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,25 +1,26 @@
 class DashboardController < ApplicationController
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
-   @User = User.all
+  @User = User.all
 
-	@maps = Map.find(:all, :conditions => [ "user_id = ?", current_user.id])
+  @maps = Map.find(:all, :conditions => [ "user_id = ?", current_user.id])
 
-	followed_maps_ids = Follow.find_all_by_follower_type_and_followable_type_and_follower_id(
-								'User','Map',current_user.id).map(&:followable_id)
-	
-	@followed_maps = Map.find(followed_maps_ids)
+  followed_maps_ids = Follow.find_all_by_follower_type_and_followable_type_and_follower_id(
+                'User','Map',current_user.id).map(&:followable_id)
+  
+  @followed_maps = Map.find(followed_maps_ids)
 
-	@new_maps = Map.find(:all, :conditions => [" created_at between ? AND ? AND private = ?", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day, false],
-			:limit => 10, :order => 'created_at DESC')
-	
+  # Hämtar kartor som är skapade fr.o.m dagens början
+  @new_maps = Map.find(:all, :conditions => [" created_at >= ? AND private = ?", Time.zone.now.beginning_of_day, false],
+      :limit => 10, :order => 'created_at DESC')
+  
   ## Alla kartor - Tillfälligt i DEV-läge
   @all_maps = Map.all
-	
-  	if connected_user = session['warden.user.twitter.connected_user.key'].present?
-  		connected_user = User.find(connected_user)
-	  # Ask user if she/he wants to merge her/his accounts
+  
+    if connected_user = session['warden.user.twitter.connected_user.key'].present?
+      connected_user = User.find(connected_user)
+    # Ask user if she/he wants to merge her/his accounts
     # (or just go ahead and merge them)
     end
 
