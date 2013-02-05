1959ef2e84f64a0a2e5d206993aaf1369ed7915f
Claes Tillborg <claes_tillborg@live.se>
2013-02-05 15:01:55 +0100

    ta bort locationType i seeds-filen

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-LocationType.create(name:'Badplats')
-LocationType.create(name:'Park')
