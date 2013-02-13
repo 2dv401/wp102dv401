610063746e5a93a1909d1a7d6dfe2f380a94022b
Claes Tillborg <claes_tillborg@live.se>
2013-02-13 15:04:48 +0100

    ändra max-längd och tillåt tom beskrivning på kartor

--- app/models/map.rb
+++ app/models/map.rb
@@ -5,7 +5,7 @@ class Map < ActiveRecord::Base
   ## Skapa api nyckel
   before_create :generate_api_key
 
-	belongs_to :user
+  belongs_to :user
   belongs_to :location
 
   has_many :marks, :dependent => :destroy
@@ -22,13 +22,13 @@ class Map < ActiveRecord::Base
   attr_accessor :longitude, :latitude
   attr_accessible :name, :description, :private, :zoom, :map_type, :location_attributes
 
-  validates	:name, :presence => true, :length => { :maximum => 45 }
-  validates	:description, :length => { :maximum => 250 }
+  validates	:name, :presence => true, :length => { :maximum => 50 }
+  validates	:description, :length => { :maximum => 15360 }
   validates :private, :inclusion => {:in => [true, false]}
   validates :map_type, :inclusion => MAP_TYPES
   validates :zoom, :numericality => { :only_integer => true }
 
-  def follow(user)  
+  def follow(user)
     @follower = Follower.new()
     @follower.save
   end
@@ -41,23 +41,23 @@ class Map < ActiveRecord::Base
     self.location.latitude
   end
 
-    ## Genererar en API nyckel på alla nya kartor.  - alla nycklar blir unika.
-    def generate_api_key
-      begin
-        api_key = SecureRandom.urlsafe_base64
-      end while Map.where(:api_key => api_key).exists?
-      self.api_key = api_key
-    end
-    def comment_count
-      self.map_comments.count
-    end
-
-    def follow_count
-      self.followers('User').count
-    end
-
-    def status_count
-      self.status_updates.count
-    end
-
+  ## Genererar en API nyckel på alla nya kartor.  - alla nycklar blir unika.
+  def generate_api_key
+    begin
+      api_key = SecureRandom.urlsafe_base64
+    end while Map.where(:api_key => api_key).exists?
+    self.api_key = api_key
   end
+  def comment_count
+    self.map_comments.count
+  end
+
+  def follow_count
+    self.followers('User').count
+  end
+
+  def status_count
+    self.status_updates.count
+  end
+
+end
